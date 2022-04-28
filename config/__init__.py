51735c902c2c564cc1825cd415ea9b4d2475d409
alonelove7 <34532101+alonelove7@users.noreply.github.com>
2022-04-28 14:54:40 +0430

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -5,5 +5,6 @@ class Config:
     API_HASH = os.getenv("api_hash","ae32caf162207865ff93b9b931b2ba54")
     CHANNEL = int( os.getenv("channel_files_chat_id","-1001792892962") )
     CHANNEL_USERNAME = os.getenv("channel_username","forjoim777888")
+    CHANNEL_USERNAME_TW = os.getenv("forjoim777888")
     TOKEN = os.getenv("token","5305336110:AAHyDsa9_exGaOTyv8qDxaw_U0Z1BOO-zYM")
     DOMAIN  = os.getenv("domain","http://d4.kimo.vip")
